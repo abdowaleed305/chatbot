d7462e907c8c5cbe5cacbcf15cd140d14e182696
abdowaleed305 <abdowork67@gmail.com>
2026-05-18 17:59:21 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+import streamlit as st
+from google import genai
+
+client = genai.Client(api_key="AIzaSyDcihNIgmUZPlgJN4M_fXbCVUC6leRVX6E")
+
+st.set_page_config(page_title="ChatBot_App")
+
+st.title("Welcome to ChatBot App 👋")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for msg in st.session_state.messages:
+    with st.chat_message(msg["role"]):
+        st.markdown(msg["content"])
+
+prompt = st.chat_input("Enter any question:")
+
+if prompt:
+    st.session_state.messages.append({
+        "role": "user",
+        "content": prompt
+    })
+
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    try:
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=prompt,
+        )
+
+        reply = response.text
+
+    except Exception as e:
+        reply = f"Error: {str(e)}"
+
+    st.session_state.messages.append({
+        "role": "assistant",
+        "content": reply
+    })
+
+    with st.chat_message("assistant"):
+        st.markdown(reply)
